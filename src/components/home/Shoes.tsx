9c2165648b2beb420b2e6f91e3087c3055b0945c
alexis-delebecque <alexis.delebecque@epitech.eu>
2021-03-22 18:14:51 +0100

    [FEAT] remove arrows

--- src/components/home/Shoes.tsx
+++ src/components/home/Shoes.tsx
@@ -9,25 +9,6 @@ export enum Direction {
     NEXT = 1,
 }
 
-interface ArrowsProps {
-    callback?: (dir: Direction) => void;
-}
-
-const Arrows: FunctionComponent<ArrowsProps> = ({ callback }) => {
-    return (
-        <div className={styles.arrows_container}>
-            <div className={styles.arrow} onClick={() => callback && callback(Direction.PREV)}>
-                PREV
-                <Icon extension={Extension.SVG} name="arrow-left" width="65px" height="25px" />
-            </div>
-            <div className={styles.arrow} onClick={() => callback && callback(Direction.NEXT)}>
-                NEXT
-                <Icon extension={Extension.SVG} name="arrow-right" width="65px" height="25px" />
-            </div>
-        </div>
-    );
-};
-
 const Shoes = (): JSX.Element => {
 
     const { model, changeCurrentModel } = useContext(GlobalContext);
@@ -38,7 +19,6 @@ const Shoes = (): JSX.Element => {
                 <img alt="shoes_logo" src={model?.path} />
                 <Icon className={styles.shoes_svg} extension={Extension.SVG} name="platform" />
             </div>
-            <Arrows callback={changeCurrentModel} />
         </div>
     );
 };
