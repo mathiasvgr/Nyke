526c983150a69f1d7d0a16d6e139825ce5024c8a
alexis-delebecque <alexis.delebecque@epitech.eu>
2021-03-22 18:09:33 +0100

    [FEAT] remove home

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,16 +1,9 @@
-import styles from './Home.module.css';
-
-import Info from './Info';
-import Shoes from './Shoes';
-import Sizes from './Sizes';
+import { containerStyle } from "../New";
 
 export default function Home() {
-
     return (
-        <div className={styles.container} >
-            <Info />
-            <Shoes />
-            <Sizes />
+        <div style={containerStyle}>
+            HOME
         </div>
     );
 }
